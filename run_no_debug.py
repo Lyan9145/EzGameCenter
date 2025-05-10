4d9e6963f6ea771740668093a1080acdffa542e9
Lyan <BEN9145@outlook.com>
2025-05-10 14:37:56 +0800

    增加一键部署

--- run_no_debug.py
+++ run_no_debug.py
@@ -1,6 +1,10 @@
 from app import *
 
 app.secret_key = "actually_secret"
+host = 'localhost'
+port = 23333
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=False, host=host, port=port)
